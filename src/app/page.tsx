7c7597d945b058bfad23e36d5231cf8c83f0c66d
jacobjmc <jacobjmc23@gmail.com>
2024-11-19 09:29:10 +1100

    updated home button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,14 @@ export default function Home() {
       <div className="flex items-center gap-4">
         <Image src={logo} alt="notes app logo" width={100} height={100} />
         <span className="text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Lightpad - AI Notes
+          Lightpad - AI Powered Notes
         </span>
       </div>
       <p className="max-w-prose text-center">
         An app for writing notes with AI interaction.
       </p>
       <Button size={"lg"} asChild>
-        <Link href="/notes">Open</Link>
+        <Link href="/notes">Try for free</Link>
       </Button>
     </main>
   );
